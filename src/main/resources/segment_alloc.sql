729822a039b35eff3b130db5887743d9c2cc7400
zfhlm <914589210@qq.com>
2021-12-03 01:45:13 +0800

    Initial commit

--- src/main/resources/segment_alloc.sql
+++ src/main/resources/segment_alloc.sql
@@ -1,7 +1,7 @@
 
 CREATE TABLE `segment_alloc` (
   `namespace` varchar(100) NOT NULL COMMENT '命名空间',
-  `max_value` bigint(19) NOT NULL COMMENT '最大已使用ID(不包括此值)',
+  `max_value` bigint(19) NOT NULL COMMENT '最大已使用ID',
   `create_time` datetime NOT NULL COMMENT '创建时间',
   `modify_time` datetime DEFAULT NULL COMMENT '更新时间',
   `version` bigint(19) NOT NULL COMMENT '版本号',
